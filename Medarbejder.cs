a52d64cfa5a2510c18bbf9db6e0211f71b469afb
simon nørregaard <dwaf@live.dk>
2020-08-28 09:48:17 +0200

    Add project files.

---
+++ Medarbejder.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimonN_Constructor_Override
+{
+    class Medarbejder
+    {
+        
+        private int MinLoen = 5000;
+        private int MaxLoen = 200000;
+        private string fornavn;
+        public string Fornavn
+        {
+            get { return fornavn; }
+            set { fornavn = value; }
+        }
+        private string efternavn;
+        public string Efternavn
+        {
+            get { return efternavn; }
+            set { efternavn = value; }
+        }
+
+        public enum Jobbetegnelse {
+            NA,
+            Direktoer,
+            Saelger,
+            Produktion,
+            Elev
+        }
+        private Jobbetegnelse jobbetegn;
+
+        public Jobbetegnelse Jobbetegn
+        {
+            get { return jobbetegn; }
+            set { if (value > 0 && value <= Jobbetegnelse.Elev) jobbetegn = (Jobbetegnelse)value;
+                else jobbetegn = 0;
+            }
+        }
+
+
+
+        private int ancinitet;
+
+        public int Ancinitet
+        {
+            get { return ancinitet; }
+            set { if(value >=0) ancinitet = value;
+            else ancinitet = 0;
+            }
+        }
+
+        private int loen;
+
+        public int Loen
+        {
+            get { return loen; }
+            set { if (value >= MinLoen && value <= MaxLoen) loen = value;
+                else if (value > MaxLoen) loen = MaxLoen;
+                else loen = MinLoen; }
+        }
+
+
+
+
+        public void RetJob(Jobbetegnelse NyBet) 
+        {
+            jobbetegn = NyBet;
+        }
+
+        public override string ToString()
+        {
+            return "Navn: " + fornavn + " " + efternavn + ", Jobbetengelse: " + jobbetegn + ", Månedsløn: " + loen + ", År i virksomhed: " + ancinitet;
+        }
+        public Medarbejder() : this("N/A", "N/A") { }
+        public Medarbejder(string ForNvn, string EftNavn) : this(ForNvn, EftNavn, 0, 0, 0) { }
+        public Medarbejder(string ForNvn, string EftNavn, Jobbetegnelse JobBet, int Ln, int Ancin)
+        {
+            Fornavn = ForNvn;
+            Efternavn = EftNavn;
+            Jobbetegn = JobBet;
+            Loen = Ln;
+            Ancinitet = Ancin;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimonN_Constructor_Override
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// Opreter reference til Medarbejder klassen og liste til at holde medarbejderne.
+            Medarbejder Medarb = new Medarbejder(); 
+            
+            // Opretter medarbejderne og indsætter dem i en liste.
+            
+                Medarbejder Med1 = new Medarbejder();
+                Medarbejder Med2 = new Medarbejder("Abe", "Torkildsen" );
+                Medarbejder Med3 = new Medarbejder("Hugo", "Rabud");
+                Medarbejder Med4 = new Medarbejder("Karsen","Kortin",Medarbejder.Jobbetegnelse.Direktoer, 210000, -2);
+                Medarbejder Med5 = new Medarbejder("Camilla", "Bundvhed", Medarbejder.Jobbetegnelse.Elev, 200, 3);
+                Medarbejder Med6 = new Medarbejder("Kryler", "Palani", Medarbejder.Jobbetegnelse.Saelger, 6000, 5);
+                List<Medarbejder> MedarbejderListe = new List<Medarbejder>() { Med1, Med2, Med3, Med4, Med5, Med6 };
+
+
+
+            //Udskriver listen med medarbejdere.
+            Console.WriteLine("Udskriver medarbejderne.");
+            foreach (var item in MedarbejderListe)
+            {
+                Console.WriteLine(item.ToString());
+
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Random Rnd = new Random();
+            Medarbejder.Jobbetegnelse[] NyJob = new Medarbejder.Jobbetegnelse[5] { Medarbejder.Jobbetegnelse.NA, Medarbejder.Jobbetegnelse.Direktoer, Medarbejder.Jobbetegnelse.Saelger, Medarbejder.Jobbetegnelse.Produktion, Medarbejder.Jobbetegnelse.Elev, };
+            foreach (var item in MedarbejderListe)
+            {
+                item.RetJob(NyJob[Rnd.Next(1,5)]);
+                Console.WriteLine(item.ToString());
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
